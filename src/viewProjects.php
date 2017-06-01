c0e5446304426d0830bd811243fc52c91cfc56a1
TheMan33 <funguy3@live.com>
2017-06-01 13:01:08 +1200

    Add files via upload

--- src/viewProjects.php
+++ src/viewProjects.php
@@ -23,53 +23,55 @@ $conn = mysqli_connect($host, $user, $pswd, $dbnm) or die("<p>Unable to connect 
 $username = $_POST["userName"];
 $password = $_POST["password"];
 
-$query = "SELECT * FROM developers WHERE username = '$username'";
-$result = mysqli_query($conn, $query) or die("<p>Unable to execute the query.</p>"
-    . "<p>Error code " . mysqli_errno($conn) . ": " . mysqli_error($conn)) . "</p>";
+if (trim($username) != "" && trim($password) != "") {
 
-if(!$result){
-    echo "The username you have entered is incorrect.";
-    die;
-}
+    $query = "SELECT * FROM developers WHERE username = '$username'";
+    $result = mysqli_query($conn, $query) or die("<p>Unable to execute the query.</p>"
+        . "<p>Error code " . mysqli_errno($conn) . ": " . mysqli_error($conn)) . "</p>";
 
-$row = mysqli_fetch_row($result);
-if($row[1] != $password){
-    echo("That password you have entered is incorrect.");
-    die;
-}
 
-$query = "SELECT * FROM projects WHERE username = '$username'";
-$result = mysqli_query($conn, $query) or die("<p>Unable to execute the query.</p>"
-    . "<p>Error code " . mysqli_errno($conn) . ": " . mysqli_error($conn)) . "</p>";
-if(!$result){
-    echo "This developer does not have any current projects.";
-    die;
-}
-$row = mysqli_fetch_row($result);
+    $row = mysqli_fetch_row($result);
+    if ($row[0] != $username) {
+        header('Location: ../index.php');
+    } else if ($row[1] != $password) {
+        header('Location: ../index.php');
+    }
 
-echo "<table style='float : left;' border=2 id = \"Projects\" align = \"center\" width = \"50%\">";
+    $query = "SELECT * FROM projects WHERE username = '$username'";
+    $result = mysqli_query($conn, $query) or die("<p>Unable to execute the query.</p>"
+        . "<p>Error code " . mysqli_errno($conn) . ": " . mysqli_error($conn)) . "</p>";
+    if (!$result) {
+        echo "This developer does not have any current projects.";
+        die;
+    }
+    $row = mysqli_fetch_row($result);
+
+    echo "<table style='float : left;' border=2 id = \"Projects\" align = \"center\" width = \"50%\">";
     echo "<tr align = \"left\" valign = \"middle\">";
-        echo "<th>Projects</th>";
+    echo "<th>Projects</th>";
     echo "</tr>";
 
-    while($row){
+    while ($row) {
         echo "<tr align = \"left\" valign = \"middle\">";
         echo "<td> <input type='button' value='$row[0]' name='$row[0]' onClick=\"allClasses('retrieveClasses.php', '$row[0]')\"> </td>";
         echo "</tr>";
         $row = mysqli_fetch_row($result);
     }
-echo "</table>";
+    echo "</table>";
 
-echo "<table style='float: right;' border=2 id = \"Classes\" align = \"center\" width = \"50%\">";
+    echo "<table style='float: right;' border=2 id = \"Classes\" align = \"center\" width = \"50%\">";
     echo "<tr align = \"left\" valign = \"middle\">";
-        echo "<th>Classes</th>";
+    echo "<th>Classes</th>";
     echo "</tr>";
 //    echo "<tableBody>";
 //    echo "</tableBody>";
 //    echo "<tr align = \"left\" valign = \"middle\">";
 //        echo "<td><span id=\"classes\"></span></td>";
 //    echo "</tr>";
-echo "</table>";
+    echo "</table>";
+} else {
+    header('Location: ../index.php');
+}
 
 ?>
 </body>
